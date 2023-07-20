9963a7f17654c61f8ef9d0d8d3a55ff5ed65694b
Andrew O'Hara <ohara.andrew314@gmail.com>
2023-07-20 19:42:10 -0400

    :art: [api] remove direct slf4j-api dependency

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -24,7 +24,6 @@ tasks.compileKotlin {
 }
 
 val kotshiVersion = "2.11.4"
-val slf4jVersion = "2.0.7"
 
 dependencies {
     implementation(platform("org.http4k:http4k-bom:5.4.0.0"))
@@ -45,8 +44,7 @@ dependencies {
     implementation("com.github.oharaandrew314:service-utils:1.5.0")
     implementation("com.nimbusds:nimbus-jose-jwt:9.26")
     implementation("io.github.oshai:kotlin-logging-jvm:4.0.2")
-    implementation("org.slf4j:slf4j-api:$slf4jVersion")
-    runtimeOnly("org.slf4j:slf4j-simple:$slf4jVersion")
+    runtimeOnly("org.slf4j:slf4j-simple:2.0.7")
     implementation("se.ansman.kotshi:api:$kotshiVersion")
     ksp("se.ansman.kotshi:compiler:$kotshiVersion")
     implementation("com.github.ksuid:ksuid:1.1.2")
