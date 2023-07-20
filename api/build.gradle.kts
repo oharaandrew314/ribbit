56bcb456bbf1cd094d876d9a2e47c3f8b9bc38e9
Andrew O'Hara <ohara.andrew314@gmail.com>
2023-07-20 19:40:39 -0400

    :art: [api] remove gradle version catalog

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -24,32 +24,36 @@ tasks.compileKotlin {
 }
 
 val kotshiVersion = "2.11.4"
+val slf4jVersion = "2.0.7"
 
 dependencies {
+    implementation(platform("org.http4k:http4k-bom:5.4.0.0"))
+    implementation(platform("org.http4k:http4k-connect-bom:5.1.3.0"))
+    implementation(platform("dev.forkhandles:forkhandles-bom:2.6.0.0"))
+
     implementation(kotlin("stdlib"))
-    implementation(libs.http4k.serverless.lambda)
-    implementation(libs.utils)
-    implementation(libs.http4k.core)
-    implementation(libs.http4k.cloudnative)
-    implementation(libs.http4k.contract)
-    implementation(libs.http4k.format.moshi) {
+    implementation("org.http4k:http4k-core")
+    implementation("org.http4k:http4k-serverless-lambda")
+    implementation("org.http4k:http4k-cloudnative")
+    implementation("org.http4k:http4k-contract")
+    implementation("org.http4k:http4k-format-moshi") {
         exclude("org.jetbrains.kotlin", "kotlin-reflect")
     }
-    implementation(libs.http4k.connect.amazon.dynamodb)
-    implementation(libs.forkhandles.result4k)
-    implementation(libs.forkhandles.values4k)
-    implementation(libs.nimbus)
+    implementation("org.http4k:http4k-connect-amazon-dynamodb")
+    implementation("dev.forkhandles:result4k")
+    implementation("dev.forkhandles:values4k")
+    implementation("com.github.oharaandrew314:service-utils:1.5.0")
+    implementation("com.nimbusds:nimbus-jose-jwt:9.26")
     implementation("io.github.oshai:kotlin-logging-jvm:4.0.2")
-    implementation(libs.slf4j.api)
-    runtimeOnly(libs.slf4j.simple)
+    implementation("org.slf4j:slf4j-api:$slf4jVersion")
+    runtimeOnly("org.slf4j:slf4j-simple:$slf4jVersion")
     implementation("se.ansman.kotshi:api:$kotshiVersion")
     ksp("se.ansman.kotshi:compiler:$kotshiVersion")
     implementation("com.github.ksuid:ksuid:1.1.2")
 
     testImplementation(kotlin("test"))
-    testImplementation(libs.kotest.assertions.core)
-    testImplementation(libs.forkhandles.result4k.kotest)
-    testImplementation(libs.http4k.testing.kotest)
-    testImplementation(libs.http4k.testing.approval)
-    testImplementation(libs.http4k.connect.amazon.dynamodb.fake)
+    testImplementation("dev.forkhandles:result4k-kotest")
+    testImplementation("org.http4k:http4k-testing-kotest")
+    testImplementation("org.http4k:http4k-testing-approval")
+    testImplementation("org.http4k:http4k-connect-amazon-dynamodb-fake")
 }
