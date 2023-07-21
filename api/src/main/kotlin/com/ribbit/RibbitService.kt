4bbc01b22a15d385f2af8af99410130825052ac3
Andrew O'Hara <ohara.andrew314@gmail.com>
2023-07-20 20:31:33 -0400

    :art: [api] switch security back to plain bearer auth

--- api/src/main/kotlin/com/ribbit/RibbitService.kt
+++ api/src/main/kotlin/com/ribbit/RibbitService.kt
@@ -35,7 +35,7 @@ import org.http4k.contract.contract
 import org.http4k.contract.openapi.ApiInfo
 import org.http4k.contract.openapi.v3.OpenApi3
 import org.http4k.contract.openapi.v3.OpenApi3ApiRenderer
-import org.http4k.contract.security.OpenIdConnectSecurity
+import org.http4k.contract.security.BearerAuthSecurity
 import org.http4k.contract.ui.swaggerUiLite
 import org.http4k.core.HttpHandler
 import org.http4k.core.Method.GET
@@ -115,10 +115,7 @@ fun RibbitService.toApi(env: Environment): HttpHandler {
     val contexts = RequestContexts()
     val authLens = RequestContextKey.required<EmailHash>(contexts, "ribbit-auth")
 
-    val security = OpenIdConnectSecurity(
-        env[Settings.jwtIssuer].path(".well-known/openid-configuration"),
-        ServerFilters.BearerAuth(authLens, authorizer)
-    )
+    val security = BearerAuthSecurity(authLens, authorizer)
 
     val api = contract {
         routes += usersApiV1(users, authLens, security)
